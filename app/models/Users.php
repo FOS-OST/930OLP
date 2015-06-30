6b361b3702617a3aca1e782d48ab5ab776140b24
hieutrieu <mrhieutrieu@gmail.com>
2015-06-30 13:41:40 -0700

    add new source code

--- app/models/Users.php
+++ app/models/Users.php
@@ -1,14 +1,139 @@
 <?php
-
-use Phalcon\Mvc\Model;
-
-class Users extends Model
+namespace Books\App\Models;
+use Phalcon\Mvc\Model\Validator\Email as Email;
+use Phalcon\Mvc\Model\Validator\PresenceOf;
+use Phalcon\Mvc\Model\Validator\Uniqueness;
+use Phalcon\Mvc\Collection;
+use MongoRegex;
+class Users extends ModelBase
 {
+    /**
+     *
+     * @var string
+     */
+    public $name;
 
-	public $id;
+    /**
+     *
+     * @var string
+     */
+    public $email;
 
-	public $name;
+    /**
+     *
+     * @var string
+     */
+    public $password;
 
-	public $email;
+    /**
+     *
+     * @var string
+     */
+    public $avatar;
 
+    /**
+     *
+     * @var string
+     */
+    public $device_token;
+
+    /**
+     *
+     * @var string
+     */
+    public $access_token;
+
+    /**
+     *
+     * @var string
+     */
+    public $remember_token;
+
+    /**
+     *
+     * @var string
+     */
+    public $phone;
+
+    /**
+     *
+     * @var integer
+     */
+    public $amount;
+
+    /**
+     *
+     * @var integer
+     */
+    public $active;
+
+    /**
+     *
+     * @var integer
+     */
+    public $status;
+    /**
+     *
+     * @var string
+     */
+    public $updated_at;
+
+    /**
+     *
+     * @var string
+     */
+    public $created_at;
+
+    /**
+     * Validations and business logic
+     *
+     * @return boolean
+     */
+    public function validation() {
+        $this->validate(
+            new Email(
+                array(
+                    'field'    => 'email',
+                    'required' => true,
+                )
+            )
+        );
+
+        $this->validate(
+            new PresenceOf(
+                array(
+                    "field"   => "name",
+                    "message" => "The name is required"
+                )
+            )
+        );
+
+        if ($this->validationHasFailed() == true) {
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * Returns table name mapped in the model.
+     *
+     * @return string
+     */
+    public function getSource()
+    {
+        return 'users';
+    }
+
+    static function buildConditions($search){
+        $searchRegex = new MongoRegex("/$search/i");
+        $conditions = array(
+            '$or' => array(
+                array('name' => $searchRegex),
+                array('email' => $searchRegex),
+                array('phone' => $searchRegex),
+            )
+        );
+        return $conditions;
+    }
 }
